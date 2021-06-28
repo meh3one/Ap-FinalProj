6b01fcd1044ba54a2bc9047cca935789ffaa0e1d
meh3one <meh3one@gmail.com>
2021-06-28 17:43:27 +0430

    designed the Admin page

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+/********************************************************************
+* Git repository ::
+*
+* special tnx to Mat_Mot && kianmjl  for Guildness
+*
+* Done By "Meh3one" As the Finale Project For Advanced Programing
+* استاد Mahdavi         University of Isfahan   7/4/1400 - 28/6/2020
+*
+* This is a Library management system with GUI
+*
+*
+* Sources :
+*
+* login page : https://unsplash.com/@matt__feeney
+* Admin Page : https://unsplash.com/@henry_be
+*
+***********************************************************************/
+
+
 #include "Login_Sign_page/login_sign_page.h"
-#include "Admin_page/adminpage.h"
 
 #include <QApplication>
 
@@ -7,17 +25,14 @@
 
 int main(int argc, char *argv[])
 {
+QApplication a(argc, argv);
 
-    QApplication a(argc, argv);
-
-    _mkdir(".\\BookFile") ;
-    _mkdir(".\\UsersFile") ;
+_mkdir(".\\BookFile") ;
+_mkdir(".\\UsersFile") ;
 
     Login_Sign_page w;
-    w.show();
 
-//    AdminPage ap ;
-//    ap.show() ;
+        w.show();
 
-    return a.exec();
+return a.exec();
 }
